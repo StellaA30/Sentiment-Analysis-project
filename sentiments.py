ce98cd16d1c106f7c415a3c13497de40035ed13f
StellaA30 <56633439+StellaA30@users.noreply.github.com>
2021-07-31 14:27:47 +0100

    Add files via upload

---
+++ sentiments.py
@@ -0,0 +1,68 @@
+import tweepy
+from textblob import Textblob
+import preprocessor as p
+import statistics
+from typing import List
+
+# consumer_key = ""
+# consumer_secret = ""
+# API KEY = 5xCirvugtA57w198C0X1wm3jd
+# # API SECRET KEYWORD : 79ijwrGDCM4EnZ37weMiU6z36kY6cr7NiUAlwjk8ZWK8W0rc4R
+
+from secrets import consumer_key, consumer_secret
+auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
+api = tweepy.API(auth)
+
+
+
+def get_tweets(keyword: str) -> List[str]:
+    all_tweets = []
+    for tweet in tweepy.Cursor(api.search, q=keyword, tweet_mode= 'extended', lang='en').items(10):
+        all_tweets.append(tweet.full_text)
+
+    return all_tweets
+
+
+#this is a function that cleans the tweets before being passed into the get sentiment function
+def clean_tweets(all_tweets: List(str)) -> List[str]:
+    tweets_clean  = []
+    for tweet in all_tweets:
+        tweets_clean.append(p.clean(tweet))
+
+    return tweets_clean
+
+
+def get_sentiment(all_tweets: List[str]) -> List[float]:
+    sentiment_scores = []
+    for tweet in all_tweets:
+        blob = Textblob(tweet)
+        sentiment_scores.append(blob.sentiment.polarity)
+
+    return sentiment_scores
+
+
+
+def generate_average_sentiment_score(keyword: str) -> int:
+    tweets = get_tweets(keyword)
+    tweets_clean = clean_tweets(tweets)
+    sentiment_scores = get_sentiment(tweets_clean)
+
+    average_score = statistics.mean(sentiment_scores)
+
+    return average_score
+
+
+if __name__ == "__main__":
+    print('What does the world prefer?')
+    first_thing  = input()
+    print("...or...")
+    second_thing = input()
+    print("\n")
+
+    first_score = generate_average_sentiment_score(first_thing)
+    second_score = generate_average_sentiment_score(second_thing)
+
+    if(first_score > second_score ):
+        print(f'The humanity prefers {first_thing} over {second_thing}!')
+    else:
+        print(f'The humanity prefers {second_thing} over {first_thing}!') 
